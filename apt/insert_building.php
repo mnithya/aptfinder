ed51fc646c3c2076e1c04caf4e74571a81059fd8
nbn6sn <nbn6sn@labunix01.cs.virginia.edu>
2016-04-20 17:38:33 -0400

    fixed building error

--- apt/insert_building.php
+++ apt/insert_building.php
@@ -8,7 +8,6 @@
 	$stmt = $db->stmt_init();
 	$name = $_POST['bldg_name'];
 	$url = $_POST['bldg_url'];
-	$max_occ = $_POST['bldg_max_occ'];
 	$walk_score = $_POST['bldg_walk_score'];
 	$rating = $_POST['bldg_rating'];
 	$pets = $_POST['bldg_pets'];
@@ -17,11 +16,10 @@
 	
 
 	//iterate through the possible attributes and see if we need to insert none into them
-	//print_r($_POST);
+	print_r($_POST);
 	/*
 	Array ( [bldg_name] => 		$name
 	[bldg_url] => 				$url
-	[bldg_max_occ] =>  			$max_occ
 	[bldg_walk_score] =>  		$walk_score
 	[bldg_rating] =>  			$rating
 	[bldg_pets] => 1  			$pets
@@ -29,10 +27,6 @@
 	[bldg_cmpy] => 				$cmpy
 	)
 	*/
-	// using empty() over isset()
-	// empty: it will return true if the variable is an empty string, false, array(), NULL, “0?, 0, and an unset variable.
-	/*INSERT INTO `Building`(`building_id`, `address_id`, `website_url`, `max_occupancy`, `walk_score`, `pets_allowed`, `rating`, `bldg-company_id`, `name`) 
-	VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8],[value-9])*/
 	if (array_key_exists('bldg_cmpy', $_POST)) 
 	{
 		$cmpy = $_POST['bldg_cmpy'];
@@ -43,10 +37,10 @@
 		$addr_id = $_POST['bldg_addr'];
 	}
 
-	if($stmt->prepare("INSERT INTO Building(address_id, website_url, max_occupancy, walk_score, pets_allowed, rating, name, `bldg-company_id`) VALUES
-		(?, ?, ?, ?, ?, ?, ?, ?)") or die("Error: You were not able to insert into the database, Sorry!"))
+	if($stmt->prepare("INSERT INTO Building(address_id, website_url, walk_score, pets_allowed, rating, name, `bldg-company_id`) VALUES
+		(?, ?, ?, ?, ?, ?, ?)") or die(header('Location: ./index.html')))
 	{
-			$stmt->bind_param(ssssssss, $addr_id, $url, $max_occ, $walk_score, $pets, $rating, $name, $cmpy);
+			$stmt->bind_param(sssssss, $addr_id, $url, $walk_score, $pets, $rating, $name, $cmpy);
 			$stmt->execute();
 			$stmt->close();
 			echo $_POST['name'] . " added on $day at $time";

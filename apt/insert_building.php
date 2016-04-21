5f8fa162c020473a3da0575a856e417674acf173
nbn6sn <nbn6sn@labunix01.cs.virginia.edu>
2016-04-20 21:39:12 -0400

    changed building insert

--- apt/insert_building.php
+++ apt/insert_building.php
@@ -1,11 +1,6 @@
 <?php
 	require "dbutil.php";
 	$db = DbUtil::loginConnection();
-	$timestamp = time();
-	date_default_timezone_set('America/New_York');
-	$day = date("Y-m-d", $timestamp);
-	$time = date("H:i:s", $timestamp);
-	$stmt = $db->stmt_init();
 	$name = $_POST['bldg_name'];
 	$url = $_POST['bldg_url'];
 	$walk_score = $_POST['bldg_walk_score'];
@@ -13,10 +8,16 @@
 	$pets = $_POST['bldg_pets'];
 	$addr_id = NULL;
 	$cmpy = NULL;
+
+	$addr_str_num = $_POST['address_street_num'];
+	$addr_str = $_POST['address_street'];
+	$addr_city = $_POST['address_city'];
+	$addr_state = $_POST['address_state'];
+	$addr_zc = $_POST['address_zipcode'];
 	
 
 	//iterate through the possible attributes and see if we need to insert none into them
-	print_r($_POST);
+	//print_r($_POST);
 	/*
 	Array ( [bldg_name] => 		$name
 	[bldg_url] => 				$url
@@ -27,23 +28,34 @@
 	[bldg_cmpy] => 				$cmpy
 	)
 	*/
+	$stmt = $db->stmt_init();
+	if($stmt->prepare("INSERT INTO Address(street, city, state, zipcode, street_num) VALUES (?, ?, ?, ?, ?)") or die(header('Location: ./error.html')))
+	{
+			$stmt->bind_param(sssss, $addr_str, $addr_city, $addr_state, $addr_zc, $addr_str_num);
+			$stmt->execute();
+			$stmt->close();
+			//echo $_POST['name'] . " added on $day at $time";
+	}
+
+	$addr_id = $db->insert_id;
+
+	if(is_null($addr_id))
+	{
+		header('Location: ./error.html');
+	}
+
+	$stmt = $db->stmt_init();
 	if (array_key_exists('bldg_cmpy', $_POST)) 
 	{
 		$cmpy = $_POST['bldg_cmpy'];
 	}
 
-	if (array_key_exists('bldg_addr', $_POST)) 
-	{	
-		$addr_id = $_POST['bldg_addr'];
-	}
-
-	if($stmt->prepare("INSERT INTO Building(address_id, website_url, walk_score, pets_allowed, rating, name, `bldg-company_id`) VALUES
-		(?, ?, ?, ?, ?, ?, ?)") or die(header('Location: ./index.html')))
+	if($stmt->prepare("INSERT INTO Building(address_id, website_url, walk_score, pets_allowed, rating, name, `bldg-company_id`) VALUES (?, ?, ?, ?, ?, ?, ?)") or die(header('Location: ./error.html')))
 	{
 			$stmt->bind_param(sssssss, $addr_id, $url, $walk_score, $pets, $rating, $name, $cmpy);
 			$stmt->execute();
 			$stmt->close();
-			echo $_POST['name'] . " added on $day at $time";
+			//echo $_POST['name'] . " added on $day at $time";
 	}
 	$db->close();
 	header('Location: ./index.html');
--- apt/update_building.php
+++ apt/update_building.php
@@ -31,12 +31,11 @@ $timestamp = time();
 					<div class='panel panel-default'>
 					<div class='panel-heading'>General Info</div>
 						<div class='panel-body' style='min-height: 300; max-height: 300; overflow-y: auto;'>
-							<input type=\"text\" name=\"bldg_name\" placeholder='Building Name' > <br/><br/>
-							<input type=\"text\" name=\"bldg_url\" placeholder='Website'> <br/><br/>
-							<input type=\"text\" name=\"bldg_max_occ\" placeholder='Max Occupancy'> <br/><br/>
-							<input type=\"text\" name=\"bldg_walk_score\" placeholder='Walk Score'> <br/><br/>
-							<input type=\"text\" name=\"bldg_rating\" placeholder='Rating'> <br/><br/>
-							Pets Allowed: <select name=\"bldg_pets\">
+							<input type=\"text\" name=\"bldg_name\" placeholder='Building Name' required> <br/><br/>
+							<input type=\"text\" name=\"bldg_url\" placeholder='Website' required> <br/><br/>
+							<input type=\"text\" name=\"bldg_walk_score\" placeholder='Walk Score' required> <br/><br/>
+							<input type=\"text\" name=\"bldg_rating\" placeholder='Rating' required> <br/><br/>
+							Pets Allowed: <select name=\"bldg_pets\" required>
 								<option value=\"1\">Yes</option>
 								<option value=\"0\">No</option>
 							</select> 
@@ -50,21 +49,14 @@ $timestamp = time();
 		<div class='panel-heading'>
 			Building Address
 			</div>
-			<div class='panel-body' style='min-height: 300; max-height: 300; overflow-y: auto;'>";
-	$stmt = $db->stmt_init();
-	if($stmt->prepare("select * from Address") or die(mysqli_error($db))) {
-		$stmt->execute();
-		$stmt->bind_result($address_id, $street, $city, $state, $zipcode, $street_num);
-		while($stmt->fetch()) {
-			echo "<input type=\"radio\" name=\"bldg_addr\" value=$address_id>";
-			echo "    $street_num $street $city, $state $zipcode";
-			echo "</br>";
-		}
-		$stmt->close();
-	}
-	else{
-		echo "Failed to retrieve addresses";
-	}
+			<div class='panel-body' style='min-height: 300; max-height: 300; overflow-y: auto;'>
+			<input type=\"text\" name=\"address_street_num\" placeholder='Street Number' required> <br/><br/>
+			<input type=\"text\" name=\"address_street\" placeholder='Street ' required> <br/><br/>
+			<input type=\"text\" name=\"address_city\" placeholder='City' required> <br/><br/>
+			<input type=\"text\" name=\"address_state\" placeholder='State' required> <br/><br/>
+			<input type=\"text\" name=\"address_zipcode\" placeholder='Zipcode' required> <br/><br/>
+			";
+	
 	echo "</div>
 	</div>
 	</div>";
